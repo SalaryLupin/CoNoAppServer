1f0717f82aed7be2ecfd173711b78c34255a2d9d
owlsogul <owlsogul@naver.com>
2019-09-24 18:17:34 +0900

    playlist api 주소

--- api/playlist/index.js
+++ api/playlist/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const playlistController = require("./playlist.controller")
 
-router.get("", playlistController.index)
+router.get("/", playlistController.index);
+router.get("/:playlistId", playlistController.show);
 
 module.exports = router
--- api/playlist/playlist.controller.js
+++ api/playlist/playlist.controller.js
@@ -5,7 +5,20 @@ exports.index = (req, res) => {
   console.log("index approach")
   models.Playlist
     .findAll()
-    .then(function(result) {
-      res.json(result)
+    .then((result) => {
+      res.json(result);
+    })
+}
+
+exports.show = (req, res) => {
+  console.log("show approach")
+  let playlistId = req.params.playlistId
+  models.Playlist
+    .findOne({
+      where: { playlistId: playlistId },
+      include: { model: models.SongList }
+    })
+    .then((result) => {
+      res.json(result);
     })
 }
