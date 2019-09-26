5c6a7324cee14a8276ae74f5bbfc91aeed785ab3
owlsogul <owlsogul@naver.com>
2019-09-26 13:02:59 +0900

    playlist 조회 url 변경

--- api/playlist/index.js
+++ api/playlist/index.js
@@ -5,10 +5,13 @@ const plc = require("./playlist.controller");
 const sc = require("./song.controller")
 
 
+// 유저가 플레이리스트를 가지고 있는지 확인
+router.use("/:playlistId", plc.getPlaylist)
+
 // 플레이리스트에 곡을 제거하는 api
 router.delete("/:playlistId/song", sc.delete);
 // 플레이리스트의 곡 정보를 가져오는 api
-router.get("/:playlistId/song/list", sc.list);
+router.get("/:playlistId/song/", sc.list);
 router.get("/:playlistId/song/:songId", sc.show);
 // 플레이리스트에 곡을 추가하는 api
 router.post("/:playlistId/song", sc.add);
@@ -20,7 +23,7 @@ router.post("/:playlistId/member", plc.inviteMember);
 // 공유된 플레이리스트에서 나가는 api
 router.delete("/:playlistId/member", plc.leavePlaylist);
 // 유저의 플레이리스트의 정보를 가져오는 api
-router.get("/list", plc.index);
+router.get("/", plc.index);
 router.get("/:playlistId", plc.show);
 // 플레이리스트를 추가하는 api
 router.post("/", plc.add);
