38720e79c58b64ee75ed04e9b1fd2ce2daba5ef0
owlsogul <owlsogul@naver.com>
2019-10-10 22:29:24 +0900

    iv 추가

--- util/coder.js
+++ util/coder.js
@@ -2,13 +2,14 @@ const crypto = require("crypto")
 const tokenConfig = require("../config/token")
 
 let encryptKey = tokenConfig.encryptKey
+let iv = crypto.randomBytes()
 
 module.exports = {
 
   /** AES256으로 암호화된 토큰을 복호화함 */
   decrypt: (encryptedString) => {
     encryptedString = encryptedString.replace(/\-/gi, "+").replace(/\_/gi, "/")
-    const decipher = crypto.createDecipher('aes-256-cbc', encryptKey);
+    const decipher = crypto.createDecipher('aes-256-cbc', encryptKey, iv);
     let result = decipher.update(encryptedString, 'base64', 'utf8');
     result += decipher.final('utf8');
     return result
@@ -16,8 +17,8 @@ module.exports = {
 
   /** AES256으로 암호화함 */
   encrypt: (originString) => {
-    const cipher = crypto.createCipher('aes-256-cbc', encryptKey);
-    let result = cipher.update(originString, 'utf8', 'base64');
+    const cipher = crypto.createCipher('aes-256-cbc', encryptKey, iv);
+    let result = cipher.update(originString, 'utf-8', 'base64');
     result += cipher.final('base64');
     result = result.replace(/\+/gi, "-").replace(/\//gi, "_").replace(/\=/gi, "")
     return result
