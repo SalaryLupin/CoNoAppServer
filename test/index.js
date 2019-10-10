f7de745c9333b195502bc196296cc0b6af3dcdde
owlsogul <owlsogul@naver.com>
2019-10-10 18:58:43 +0900

    en/decoder 테스트 api 만듬

--- test/index.js
+++ test/index.js
@@ -3,10 +3,11 @@ const router = express.Router()
 const coder = require("../util/coder")
 
 router.post("/decode", (req, res) => {
-
   res.json({ result: coder.decrypt(req.body.string) })
+})
 
-
+router.post("/encode", (req,res) => {
+  res.json({ result: coder.encrypt(req.body.string) })
 })
 
 module.exports = router
